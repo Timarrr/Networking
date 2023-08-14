1892af1eb89fb68c5b69806eb177aa1b2d9e25c2
Nikita Sapsay <nikita_sapsay@cashalot.co>
2023-08-14 21:12:41 +1000

    file transfer mechanism

--- fileTransfer/client.cpp
+++ fileTransfer/client.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #define PORT 55000
-  
+void createFile(char* array_ch)
+{
+    std::stringstream code{array_ch};
+    std::string filename_ = "";
+    std::string size_ = "";
+    std::getline(code, filename_, '\0');
+    std::getline(code, size_, '\0');
+    std::ofstream file(filename_, std::ios::binary);
+    std::string line;
+    while(std::getline(code, line, '\0'))	file << line;
+}
 int main(int argc, char const* argv[])
 {
     int status, valread, client_fd;
@@ -38,6 +52,7 @@ int main(int argc, char const* argv[])
     std::cout << "Hello message sent\n";
     valread = read(client_fd, buffer, 65535);
     std::cout << std::string(buffer);
+
     // closing the connected socket
     close(client_fd);
     shutdown(client_fd, SHUT_RDWR);
